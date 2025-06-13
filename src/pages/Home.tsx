2e69ebd2ca9b9cc084971e99a3e19ff94776a4f6
arman7rm <arman97rm@gmail.com>
2025-06-13 16:39:34 -0400

    added footer

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Footer from "../components/Footer/Footer";
 import Header from "../components/Header/Header";
 import MobileMenu from "../components/MobileMenu";
 
@@ -6,6 +7,7 @@ const Home = () => {
     <>
       <MobileMenu />
       <Header />
+      <Footer />
     </>
   );
 };
