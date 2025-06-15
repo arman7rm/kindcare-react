bad61ea7f07b3ff7437d3f08d79d14e264372185
arman7rm <arman97rm@gmail.com>
2025-06-14 21:01:55 -0400

    changes

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,6 +1,4 @@
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import MobileMenuNav from "../components/MobileMenuNav";
+
 
 const Careers = () => {
   return (
