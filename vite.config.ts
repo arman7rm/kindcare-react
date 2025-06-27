1076a72eea779928d26f1a6d80ad31c50efdbb5b
arman7rm <arman97rm@gmail.com>
2025-06-27 16:21:54 -0400

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/kindcare-react/', // <--- Add this line
+  base: '/', // <--- Add this line
   plugins: [tailwindcss(), react()],
 });
